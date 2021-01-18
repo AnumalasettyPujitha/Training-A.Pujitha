a6abf6e87422b9a0ae615a8f352892802cc3605f
anu <anu@Saipoojitha>
2021-01-18 16:43:23 +0530

    hibernate inheritance with union subclass

---
+++ UnionSubClass/src/com/transaction/CreditCard.java
@@ -0,0 +1,23 @@
+package com.transaction;
+
+public class CreditCard extends Payment {
+	private int card_number;
+	private String card_type;
+
+	public String getCard_type() {
+		return card_type;
+	}
+
+	public void setCard_type(String card_type) {
+		this.card_type = card_type;
+	}
+
+	public int getCard_number() {
+		return card_number;
+	}
+
+	public void setCard_number(int card_number) {
+		this.card_number = card_number;
+	}
+	
+}
---
+++ UnionSubClass/src/com/transaction/Payment.java
@@ -0,0 +1,22 @@
+package com.transaction;
+
+public class Payment {
+	private int payment_id,amount;
+
+	public int getPayment_id() {
+		return payment_id;
+	}
+
+	public void setPayment_id(int payment_id) {
+		this.payment_id = payment_id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+}

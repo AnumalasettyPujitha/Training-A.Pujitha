38abccf6486187c4cc1444130dc69e4a3de6598a
Training/A.Pujitha <73580049+AnumalasettyPujitha@users.noreply.github.com>
2021-02-08 22:46:11 +0530

    Spring Jdbc Without Annotations

---
+++ SpringJdbc/src/com/student/StudentDao.java
@@ -0,0 +1,36 @@
+package com.student;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+public class StudentDao {
+	private JdbcTemplate jdbcTemplate;  
+	  
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {  
+	    this.jdbcTemplate = jdbcTemplate;  
+	}  
+	  
+	public int saveStudent(Student s){  
+	    String query="insert into student values('"+s.getId()+"','"+s.getName()+"','"+s.getAge()+"')";  
+	    return jdbcTemplate.update(query);  
+	} 
+	public List<Student> getAllStudents(){  
+		 List li=jdbcTemplate.queryForList("select * from student");
+		 return li;
+	 } 
+	public int updateStudent(int id,int age) {
+		String query="update student set age='"+age+"' where id='"+id+"'";
+		return jdbcTemplate.update(query);
+	}
+	public int deleteStudent(int id) {
+		String query="delete from student where id='"+id+"'";
+		return jdbcTemplate.update(query);
+	}
+}  
+
